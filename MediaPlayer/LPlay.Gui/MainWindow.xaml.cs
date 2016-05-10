45abef39e4f4bb16f30c0134bd81fbf9190b3e3f
Laggson <m.estrich98@gmail.com>
2016-05-10 10:36:40 +0200

    Started Remake of the whole shit x.x

---
+++ MediaPlayer/LPlay.Gui/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using LPlay.Logic;
+using System;
+
+namespace LPlay.Gui
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        Class1 _con;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _con = new Class1();
+        }
+
+        private void MetroWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.Title = "LPlay v" + _con.GetVersion();
+        }
+    }
+}
---
+++ MediaPlayer/LPlay.Logic/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace LPlay.Logic
+{
+    public class Class1
+    {
+        public string GetVersion()
+        {
+            System.Version ver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            return ver.Major + "." + ver.Minor + "." + ver.Build;
+        }
+    }
+}
