b01fec5fdb8187a01aa4901365d99cb21b300466
Laggson <m.estrich98@gmail.com>
2016-05-10 14:49:30 +0200

    New Interface! - Added GUI for MainWindow, this time with controls on top - Added NAudio NuGet for Logic Project

---
+++ MediaPlayer/LPlay.Gui/AnacondaControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LPlay.Gui
+{
+    /// <summary>
+    /// Interaktionslogik für AnacondaControl.xaml
+    /// </summary>
+    public partial class AnacondaControl : UserControl
+    {
+        public AnacondaControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSkipPrevious_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPlayPause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSkipNext_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnMute_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SldVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void SldTimeline_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
